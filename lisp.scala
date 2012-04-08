35f0eace818c4e05a868746c8eec942ce2a56b61
César López-Natarén <cesar@ciencias.unam.mx>
2012-04-07 23:58:18 -0700

    Implement car, cdr, cons, ff, subst, equal, null

--- lisp.scala
+++ lisp.scala
@@ -21,15 +21,18 @@
 
 import scala.util.parsing.combinator._
 import scala.collection.mutable._
+import scala.util.control.Exception._
 
 abstract class Expr
 case class Exprs(members: List[Expr]) extends Expr
 case class Symbol(name: String) extends Expr
 case class Number(num: Double) extends Expr
 
+class UndefinedException extends Exception
+
 class LispParser extends RegexParsers {
 	def expr: Parser[Expr] = symbol | ("("~>rep1(expr, expr)<~")" ^^ { x => Exprs(x) })
-	def symbol: Parser[Expr] = ("""\d+""".r ^^ { x => Number(x.toDouble) }) | ("""\w+""".r ^^ { x => Symbol(x) })
+	def symbol: Parser[Expr] = ("""\d+""".r ^^ { x => Number(x.toDouble) })	| ("""\w+""".r ^^ { x => Symbol(x) })
 	def parse(program: String): Expr = parseAll(expr, program).get
 }
 
@@ -39,18 +42,35 @@ class LispInterpreter {
 		case Symbol(_) => true
 		case _ => false
 	}
-	
 	def eq(x: Expr, y: Expr): Boolean = atom(x) && atom(y) && x == y
-	
+	def car(expr: Expr): Expr = expr match {
+		case Exprs(members) => if(members.length == 0) throw new UndefinedException else members(0)
+		case _ => throw new UndefinedException
+	}
+	def cdr(expr: Expr): Expr = expr match {
+		case Exprs(members) => if(members.length == 0) throw new UndefinedException else Exprs(members.drop(1))
+		case _ => throw new UndefinedException
+	}
+	def cons(x: Expr, y: Expr): Expr = Exprs(List(x, y))
+	def ff(expr: Expr): Expr = expr match {
+		case Exprs(members) => ff(car(expr))
+		case _ => expr
+	}
+	def subst(x: Expr, y: Expr, z: Expr): Expr =
+		if(atom(z))
+		 	if(eq(z, y))
+				x
+			else z
+		else z match {
+			case Exprs(members) => if(members.length == 0) z else cons(subst(x, y, car(z)), subst(x, y, cdr(z)))
+		}
+	def equal(x: Expr, y: Expr): Boolean =
+		((atom(x) && atom(y) && eq(x, y)) ||
+		 !atom(x) && !atom(y) && equal(car(x), car(y)) && equal(cdr(x), cdr(y)))
+	def nulll(x: Expr): Boolean = atom(x) && eq(x, Symbol("nil"))
 	def eval(expr: Expr): Any = eval(expr, new HashMap[Symbol, Expr])
-	
 	def eval(expr: Expr, env: HashMap[Symbol, Expr]): Any = expr match {
 		case Number(n) => n
 		case _ =>
 	}
 }
-
-val i = new LispInterpreter
-val p = new LispParser
-i.eq(p.parse("hola"), p.parse("hola"))
-i.eq(p.parse("hola"), p.parse("hola1"))
