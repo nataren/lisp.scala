d9ba3770338d0c50256f38c7175d6f694a0a1547
César López-Natarén <cesar@ciencias.unam.mx>
2012-04-17 22:36:04 -0700

    Tweak the internal representation, and implement more methods

--- lisp.scala
+++ lisp.scala
@@ -24,16 +24,29 @@ import scala.collection.mutable._
 import scala.util.control.Exception._
 
 abstract class Expr
-case class Exprs(members: List[Expr]) extends Expr
+case class Exprs(head:Expr, tail: Expr) extends Expr
 case class Symbol(name: String) extends Expr
 case class Number(num: Double) extends Expr
 
+class Env(p: Env) extends HashMap[Symbol, Expr] {
+	val parent = p
+	def find(e: Expr) {
+		getOrElse(e, parent.find(e))
+	}
+}
 class UndefinedException extends Exception
 
 class LispParser extends RegexParsers {
-	def expr: Parser[Expr] = symbol | ("("~>rep1(expr, expr)<~")" ^^ { x => Exprs(x) })
+	def expr: Parser[Expr] = symbol | ("("~>rep1(expr, expr)<~")" ^^ { x => expand(x) })
 	def symbol: Parser[Expr] = ("""\d+""".r ^^ { x => Number(x.toDouble) })	| ("""\w+""".r ^^ { x => Symbol(x) })
 	def parse(program: String): Expr = parseAll(expr, program).get
+	
+	// utility functions
+	def expand(l: List[Expr]): Expr = {
+		if(l.length == 1)
+			Exprs(l.head, Symbol("nil"))
+		else Exprs(l.head, expand(l.tail))
+	}
 }
 
 class LispInterpreter {
@@ -44,16 +57,16 @@ class LispInterpreter {
 	}
 	def eq(x: Expr, y: Expr): Boolean = atom(x) && atom(y) && x == y
 	def car(expr: Expr): Expr = expr match {
-		case Exprs(members) => if(members.length == 0) throw new UndefinedException else members(0)
+		case Exprs(head, _) => head
 		case _ => throw new UndefinedException
 	}
 	def cdr(expr: Expr): Expr = expr match {
-		case Exprs(members) => if(members.length == 0) throw new UndefinedException else Exprs(members.drop(1))
+		case Exprs(head, tail) => tail
 		case _ => throw new UndefinedException
 	}
-	def cons(x: Expr, y: Expr): Expr = Exprs(List(x, y))
+	def cons(x: Expr, y: Expr): Expr = Exprs(x, y)
 	def ff(expr: Expr): Expr = expr match {
-		case Exprs(members) => ff(car(expr))
+		case Exprs(head, tail) => ff(head)
 		case _ => expr
 	}
 	def subst(x: Expr, y: Expr, z: Expr): Expr =
@@ -61,16 +74,48 @@ class LispInterpreter {
 		 	if(eq(z, y))
 				x
 			else z
-		else z match {
-			case Exprs(members) => if(members.length == 0) z else cons(subst(x, y, car(z)), subst(x, y, cdr(z)))
-		}
+		else cons(subst(x, y, car(z)), subst(x, y, cdr(z)))
 	def equal(x: Expr, y: Expr): Boolean =
 		((atom(x) && atom(y) && eq(x, y)) ||
 		 !atom(x) && !atom(y) && equal(car(x), car(y)) && equal(cdr(x), cdr(y)))
 	def nulll(x: Expr): Boolean = atom(x) && eq(x, Symbol("nil"))
-	def eval(expr: Expr): Any = eval(expr, new HashMap[Symbol, Expr])
-	def eval(expr: Expr, env: HashMap[Symbol, Expr]): Any = expr match {
+	def append(x: Expr, y: Expr): Expr = if(nulll(x)) y else cons(car(x), append(cdr(x), y))
+	def eval(expr: Expr): Any = eval(expr, Env(null))
+	def eval(expr: Expr, env: Env): Any = expr match {
 		case Number(n) => n
-		case _ =>
+		case Symbol(s) => env.find(expr)
+		case _ => {
+			val cr = car(expr)
+			if(atom(cr)) {
+				if(eq(cr, Symbol('quote')))
+					car(cdr(expr))
+				else if(eq(cr, Symbol('atom')))
+					atom(eval(car(cdr(expr)), env))
+				else if(eq(cr, Symbol('eq')))
+					eval(car(cdr(expr)), env) == eval(car(cdr(cdr(expr))), env)
+				else if(eq(cr, Symbol('cond')))
+					evalCond(cdr(expr), env)
+				else if(eq(cr, Symbol('car')))
+					car(eval(car(cdr(expr)), env))
+				else if(eq(cr, Symbol('cdr')))
+					cdr(eval(car(cdr(expr)), env))
+				else if(eq(cr, Symbol('cons')))
+					cons(eval(car(cdr(expr)), env), eval(car(cdr(cdr(expr))), env))
+			} else if(eq(car(car(expr)), Symbol('label'))) {
+				eval(cons(car(cdr(cdr(car(expr)))), cdr(expr)), env += car(cdr(car(expr))) -> car(expr)))
+			} else if(eq(car(car(expr)), Symbol('lambda'))) {
+				eval(car(cdr(cdr(car(expr)))), )
+			}
+		}
+	}
+	def evalCond(cond: Expr, env: Env) {
+		if(eval(car(car(cond)))) {
+			eval(car(cdr(car(cond))), env)
+		} else {
+			evalCond(cdr(cond), env)
+		}
+	}
+	def evList(expr: Expr) {
+		
 	}
 }
